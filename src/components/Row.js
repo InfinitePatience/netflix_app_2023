7bfb510a9b13d436ac396c1f0b01b011c12539dc
InfinitePatience <roysjk34@gmail.com>
2023-04-13 10:17:28 +0900

    first commit

--- src/components/Row.js
+++ src/components/Row.js
@@ -3,6 +3,13 @@ import React, { useEffect, useState } from 'react';
 import "styles/Row.css";
 import MovieModal from './MovieModal';
 
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
 function Row({isLargeRow, title, id, fetchUrl}) {
   const [movies, setMovies] = useState([]);
   const [modalOpen, setModalOpen] = useState(false);
@@ -27,14 +34,37 @@ function Row({isLargeRow, title, id, fetchUrl}) {
   return (
     <section className='row'>
       <h2>{title}</h2>
-      <div className='slider'>
+      <Swiper
+      // install Swiper modules
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      navigation  // arrow 버튼 사용유무
+      pagination={{ clickable: true }} // 페이지 버튼 보이게 할지
+      loop={true}
+      breakpoints={{
+        1378:{
+          slidesPerView:6, // 한번에 보이는 슬라이드 개수
+          slidesPerGroup:6, // 몇개씩 슬라이드 할지
+        },998:{
+          slidesPerView:5,
+          slidesPerGroup:5,
+        },625:{
+          slidesPerView:4,
+          slidesPerGroup:4,
+        },0:{
+          slidesPerView:3,
+          slidesPerGroup:3,
+        }}}
+      >
+      {/* <div className='slider'>
         <div className='slider__arrow left'>
           <span className='arrow' onClick={() => {document.getElementById(id).scrollLeft -= (window.innerWidth - 80);}}>
            {"<"}
           </span>
-        </div>
+        </div> */}
+        
         <div id={id} className='row__posters'>
          {movies.map((movie) => (
+          <SwiperSlide>
           <img
           key={movie.id}
           onClick={() => handleClick(movie)}
@@ -43,18 +73,20 @@ function Row({isLargeRow, title, id, fetchUrl}) {
           loading='lazy'
           alt={movie.title || movie.name || movie.original_name}
           />
+          </SwiperSlide>
          ))}
         </div>
-        <div className='slider__arrow right'>
+        {/* <div className='slider__arrow right'>
           <span className='arrow' onClick={() => {document.getElementById(id).scrollLeft += (window.innerWidth - 80);}}>
             {">"}
           </span>
         </div>
-      </div>
+      </div> */}
 
       {modalOpen && (
         <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
       )}
+      </Swiper>
     </section>
   )
 }
